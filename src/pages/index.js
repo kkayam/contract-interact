641076e2d18d3962be274f44c23cead5e48d2263
kkayam <korre.remix@gmail.com>
2023-03-28 20:18:07 +0100

    read and write columns

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,33 +43,64 @@ export default function Home() {
             </h2>{implementationContract.name && (<h2 onClick={() => setViewImplementation(true)} className={!viewImplementation ? 'contract-name' : 'contract-name selected-name'}>{implementationContract.name}</h2>)}
           </div>
           {/* Render buttons and input fields for each function in the ABI */}
-          {targetContract.abi
-            .filter((item) => item.type === 'function')
-            .map((func) => (
-              <div className="function-container" key={getUniqueFuncName(func)}>
-                <div className="function-row">
-                  <div className='function-title'>
-                    <p className={(func.stateMutability == "view") ? "function-title read" : "function-title write"}>{func.name}</p>
-                  </div>
-                  {func.inputs.length > 0 &&
-                    <div className='function-inputs'>
-                      {func.inputs.map((input, index) => (
-                        <input key={index} className="interact-input" id={func.name + "." + input.name} placeholder={`${input.name} (${input.type})`} />
-                      ))}
-                    </div>}
-                  <button className="interact-button" onClick={() => handleInteract(func)}>Interact</button>
-                </div>
-                <div className='func-result'>
-                  {result[getUniqueFuncName(func)] &&
-                    (result[getUniqueFuncName(func)].type == "result") ?
-                    result[getUniqueFuncName(func)].response.map((r, i) => <p dangerouslySetInnerHTML={{ __html: ((func.outputs[i] && func.outputs[i].name) ? func.outputs[i].name + " " : "") + formatSolidityData(r) }}></p>) : ""}
+          <div className='function-type-row'>
+            <div className='function-type-container'>
+              {targetContract.abi
+                .filter((item) => item.type === 'function' && item.stateMutability == "view")
+                .map((func) => (
+                  <div className="function-container" key={getUniqueFuncName(func)}>
+                    <div className="function-row">
+                      <div className='function-title'>
+                        <p className={(func.stateMutability == "view") ? "function-title read" : "function-title write"}>{func.name}</p>
+                      </div>
+                      {func.inputs.length > 0 &&
+                        <div className='function-inputs'>
+                          {func.inputs.map((input, index) => (
+                            <input key={index} className="interact-input" id={func.name + "." + input.name} placeholder={`${input.name} (${input.type})`} />
+                          ))}
+                        </div>}
+                      <button className="interact-button" onClick={() => handleInteract(func)}>Interact</button>
+                    </div>
+                    <div className='func-result'>
+                      {result[getUniqueFuncName(func)] &&
+                        (result[getUniqueFuncName(func)].type == "result") ?
+                        result[getUniqueFuncName(func)].response.map((r, i) => <p dangerouslySetInnerHTML={{ __html: ((func.outputs[i] && func.outputs[i].name) ? func.outputs[i].name + " " : "") + formatSolidityData(r) }}></p>) : ""}
 
-                  {result[getUniqueFuncName(func)] && (result[getUniqueFuncName(func)].type == "error") ?
-                    result[getUniqueFuncName(func)].response.map((r, i) => <p className='status'>{r}</p>) : ""}
-                </div>
-              </div>
-            ))
-          }
+                      {result[getUniqueFuncName(func)] && (result[getUniqueFuncName(func)].type == "error") ?
+                        result[getUniqueFuncName(func)].response.map((r, i) => <p className='status'>{r}</p>) : ""}
+                    </div>
+                  </div>
+                ))
+              }</div>
+            <div className='function-type-container'>
+              {targetContract.abi
+                .filter((item) => item.type === 'function' && item.stateMutability != "view")
+                .map((func) => (
+                  <div className="function-container" key={getUniqueFuncName(func)}>
+                    <div className="function-row">
+                      <div className='function-title'>
+                        <p className={(func.stateMutability == "view") ? "function-title read" : "function-title write"}>{func.name}</p>
+                      </div>
+                      {func.inputs.length > 0 &&
+                        <div className='function-inputs'>
+                          {func.inputs.map((input, index) => (
+                            <input key={index} className="interact-input" id={func.name + "." + input.name} placeholder={`${input.name} (${input.type})`} />
+                          ))}
+                        </div>}
+                      <button className="interact-button" onClick={() => handleInteract(func)}>Interact</button>
+                    </div>
+                    <div className='func-result'>
+                      {result[getUniqueFuncName(func)] &&
+                        (result[getUniqueFuncName(func)].type == "result") ?
+                        result[getUniqueFuncName(func)].response.map((r, i) => <p dangerouslySetInnerHTML={{ __html: ((func.outputs[i] && func.outputs[i].name) ? func.outputs[i].name + " " : "") + formatSolidityData(r) }}></p>) : ""}
+
+                      {result[getUniqueFuncName(func)] && (result[getUniqueFuncName(func)].type == "error") ?
+                        result[getUniqueFuncName(func)].response.map((r, i) => <p className='status'>{r}</p>) : ""}
+                    </div>
+                  </div>
+                ))
+              }</div>
+          </div>
         </div >
       );
     }
